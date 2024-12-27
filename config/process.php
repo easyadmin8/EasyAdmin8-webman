61de246705c7078a1793a1ff59f76bbea27674f6
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2024-12-27 13:50:29 +0800

    feat(config): integrate Webman framework and adjust server settings
    - Add Webman framework configuration to process.php
    - Reduce max_package_size in server.php
    - Remove unnecessary server settings from server.php

--- config/process.php
+++ config/process.php
@@ -12,16 +12,36 @@
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
+use support\Log;
+use support\Request;
+use app\process\Http;
+
 global $argv;
 
 return [
+    'webman'  => [
+        'handler'     => Http::class,
+        'listen'      => 'http://0.0.0.0:' . env('APP_PORT', 8787),
+        'count'       => cpu_count() * 4,
+        'user'        => '',
+        'group'       => '',
+        'reusePort'   => false,
+        'eventLoop'   => '',
+        'context'     => [],
+        'constructor' => [
+            'requestClass' => Request::class,
+            'logger'       => Log::channel('default'),
+            'appPath'      => app_path(),
+            'publicPath'   => public_path()
+        ]
+    ],
     // File update detection and automatic reload
     'monitor' => [
-        'handler' => app\process\Monitor::class,
-        'reloadable' => false,
+        'handler'     => app\process\Monitor::class,
+        'reloadable'  => false,
         'constructor' => [
             // Monitor these directories
-            'monitorDir' => array_merge([
+            'monitorDir'        => array_merge([
                 app_path(),
                 config_path(),
                 base_path() . '/process',
@@ -33,8 +53,8 @@ return [
             'monitorExtensions' => [
                 'php', 'html', 'htm', 'env'
             ],
-            'options' => [
-                'enable_file_monitor' => !in_array('-d', $argv) && DIRECTORY_SEPARATOR === '/',
+            'options'           => [
+                'enable_file_monitor'   => !in_array('-d', $argv) && DIRECTORY_SEPARATOR === '/',
                 'enable_memory_monitor' => DIRECTORY_SEPARATOR === '/',
             ]
         ]
