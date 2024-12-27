a8610d3ac327f89ee36fcf4df30825f2f851002e
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2024-12-27 16:55:42 +0800

    fix(webman/domain): improve domain binding check case-insensitivity
    - Update domain binding check to convert domain keys to lowercase
    - Enhance compatibility with different case variations in domain names

--- config/plugin/webman/domain/app.php
+++ config/plugin/webman/domain/app.php
@@ -7,11 +7,12 @@ return [
     // 多应用绑定关系
     'bind'   => [
         // '网站默认地址，例如 www.baidu.com'     => '',
-        '127.0.0.1'       => '',
+        '127.0.0.1'            => '',
         $admin['admin_domain'] => 'admin', // 绑定到admin应用
     ],
     // 绑定关系，域名，应用的验证逻辑，返回true时认为符合绑定关系，反之不符合返回404
     'check'  => function ($bind, $domain, $app) {
-        return isset($bind[$domain]) && $bind[$domain] === $app;
+        $bind = array_change_key_case($bind, CASE_LOWER);
+        return isset($bind[strtolower($domain)]) && $bind[strtolower($domain)] === $app;
     }
 ];
