193f6911ac689f20bd9ec494aa876338d10d2d87
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2025-03-27 11:35:40 +0800

    🚀 Layui v2.10.1

--- public/static/plugs/easy-admin/easy-admin.js
+++ public/static/plugs/easy-admin/easy-admin.js
@@ -973,8 +973,9 @@ define(["jquery", "tableSelect"], function ($, tableSelect) {
                 maxmin: true,
                 anim: 0,
                 moveOut: true,
-                move: false,
+                shade: 0.3,
                 shadeClose: shadeClose,
+                scrollbar: false,
                 before: function () {
                 },
                 success: function (layero, index) {
